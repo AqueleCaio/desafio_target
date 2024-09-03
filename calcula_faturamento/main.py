481515d93434f2941c8d2cfc588f06796259f46c
AqueleCaio <106452341+AqueleCaio@users.noreply.github.com>
2024-09-03 19:57:54 -0300

    Update main.py

--- calcula_faturamento/main.py
+++ calcula_faturamento/main.py
@@ -11,10 +11,11 @@ def calcular_faturamento(dados):
     media_faturamento = np.mean(faturamentos_validos)
     dias_acima_media = np.sum(faturamentos_validos > media_faturamento)
 
+    print()
     print(f"Menor valor de faturamento: R$ {menor_faturamento:.2f}")
     print(f"Maior valor de faturamento: R$ {maior_faturamento:.2f}")
     print(f"Número de dias com faturamento acima da média: {dias_acima_media}")
-
+    print()
 # Verifica se o arquivo existe
 file_path = 'dados.json'
 
